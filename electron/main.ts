688727ae939978986969a2e1db1da152cd0325b9
Sandi Yen <sandi.yen@gmail.com>
2025-10-06 10:37:50 +0100

    Initial commit: Set up Electron + React + TypeScript project

---
+++ electron/main.ts
@@ -0,0 +1,36 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  // In development, load from the Vite dev server
+  if (process.env.NODE_ENV === 'development') {
+    win.loadURL('http://localhost:5173');
+    win.webContents.openDevTools();
+  } else {
+    // In production, load the built index.html file
+    win.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
